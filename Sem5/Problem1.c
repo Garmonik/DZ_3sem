9ff606399c0f758e9d117ac10a6622e19c27a0bb
MarkKolyadich <70436486+Garmonik@users.noreply.github.com>
2021-10-02 20:51:50 +0300

    Add files via upload

---
+++ Sem5/Problem1.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+#define BUF_SIZE 4096
+
+int num_client(char* name1, char* name2);
+
+int main(int argc, char** argv){
+    
+    if(strcmp(argv[1], "0") == 0)
+        num_client("aaa.fifo", "bbb.fifo");
+    else if(strcmp(argv[1], "1") == 0)
+        num_client("bbb.fifo", "aaa.fifo");
+            
+    return 0;
+}
+
+int num_client(char* name1, char* name2){    
+    int fd;
+    size_t size;
+    char* resstring;
+    char* str;
+    
+    resstring = (char*)calloc(BUF_SIZE, sizeof(char));
+    str = (char*)calloc(BUF_SIZE, sizeof(char));
+    
+    (void)umask;
+    
+    mknod(name1, S_IFIFO|0666, 0);
+
+    if(fork() < 0){
+         printf("Error1!");
+         exit(-1);
+    }
+    else if(fork() > 0){ 
+        fd = open(name1, O_WRONLY);
+
+        if(fd < 0){
+            printf("Error2!");
+            exit(-1);
+        }
+
+            while(1){
+                fgets(str, BUF_SIZE, stdin);
+                
+                int n = strlen(str);
+                write(fd, str, n);
+            }
+    close(fd);
+    }
+    else{
+        if((fd = open(name2, O_RDONLY)) < 0){
+            printf("Error3!");
+            exit(-1);
+        }
+
+        while(1){
+            size = read(fd, resstring, BUF_SIZE);
+     
+            printf("%s", resstring);
+            }
+            close(fd);
+    }
+    free(str);
+    free(resstring);
+
+    return 0;
+}
---
+++ Sem5/Problem1_2.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+ 
+#define BUF_SIZE 4096
+
+int main(){
+
+    int fd;
+    size_t size;
+    char* resstring;
+    char name1[] = "bbb.fifo";
+    char name2[] = "aaa.fifo";
+    char* str;
+
+    resstring = (char*)calloc(BUF_SIZE, sizeof(char));
+    str = (char*)calloc(BUF_SIZE, sizeof(char));
+
+    (void)umask;
+
+    mknod(name2, S_IFIFO|0666, 0);
+    
+    if(fork() < 0){
+        printf("Error1!");
+        exit(-1);
+    }
+    else if(fork() > 0){
+        fd = open(name2, O_WRONLY);
+
+        if(fd < 0){
+            printf("Error2!");
+            exit(-1);
+        }
+
+            while(1){
+                fgets(str, BUF_SIZE, stdin);
+
+                int n = strlen(str);
+                write(fd, str, n);
+            }
+    close(fd);
+    }
+    else{
+        if((fd = open(name1, O_RDONLY)) < 0){
+            printf("Error3!");
+            exit(-1);
+        }
+
+        while(1){
+            size = read(fd, resstring, BUF_SIZE);
+
+            printf("%s\n", resstring);
+            }
+            close(fd);
+    }
+    return 0;
+    free(str);
+    free(resstring);
+}
