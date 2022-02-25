6476dc72b1e4777c759bb099324c2f7069eb0e7d
MarkKolyadich <70436486+Garmonik@users.noreply.github.com>
2022-02-25 15:48:53 +0300

    Add files via upload

---
+++ sem4-1/Vector/Source.cpp
@@ -0,0 +1,16 @@
+#include "Vector2.h"
+
+int main(){
+	Vector2 a;
+	Vector2 b;
+
+	double c;
+	std::cin >> a;
+	std::cin >> b;
+
+	c = a.angle(b);
+
+	std::cout << c << "\n";
+
+	system("pause");
+}
